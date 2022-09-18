6f5f01c292180f384948f59e9893c9b3068a4387
Amar <113778863+Amar-Hiremath@users.noreply.github.com>
2022-09-18 17:03:20 +0530

    Create Demonstration of ternary operator.js

---
+++ Demonstration of ternary operator.js
@@ -0,0 +1,6 @@
+// Print " you can drive" or " you cannot drive" based on age being greater than 18 using ternary operator 
+
+let age = prompt ("Enter your age")
+age = Number.parseInt(age);
+let a =  (age >=18) ? "You can drive" : "You cannot drive"
+console.log(a)
