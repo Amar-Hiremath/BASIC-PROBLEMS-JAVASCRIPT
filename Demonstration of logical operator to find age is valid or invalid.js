e46e9665d79656c1a405a2b4df4f47cde7ee4e8d
Amar <113778863+Amar-Hiremath@users.noreply.github.com>
2022-09-18 17:15:44 +0530

    Create Demonstration of logical operator to find age is valid or invalid.js

---
+++ Demonstration of logical operator to find age is valid or invalid.js
@@ -0,0 +1,11 @@
+// Use logical operators to find whether the age of person lies between 10 and 20 
+
+let age = prompt("Enter your age")
+age = Number.parseInt(age);
+if (age >= 10 && age <= 20){
+  console.log ("vaild age")
+}
+else{
+  console.log ("Invaild age")
+  
+}
