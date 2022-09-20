190f02b28687cb0b9ae3e16b44e856227f327786
Amar <113778863+Amar-Hiremath@users.noreply.github.com>
2022-09-21 00:41:22 +0530

    Create Demonstrating Includes, startsWith and endsWith function.js

---
+++ Demonstrating Includes, startsWith and endsWith function.js
@@ -0,0 +1,14 @@
+//Explore the includes function 
+const sentence =  `Hey it's amar, how is it going`
+const word = `amar`
+console.log(`the word " ${word} "${sentence.includes(word) ? ` is` : `isnot`} in the sentence`);
+
+// Explore startsWith function
+const sentence = `amar hiremath is a good stock market trader`;
+const word = `amar`;
+console.log(` the sentence "${sentence.startsWith(word)? `does` : `does not`}" starts with  "${word}" `);
+
+//Explore endsWith function
+const sentence = `amar hiremath is a good stock market trader`;
+const word = `trader`;
+console.log(` the sentence "${sentence.endsWith(word)? `does` : `does not`}" starts with  "${word}" `);
