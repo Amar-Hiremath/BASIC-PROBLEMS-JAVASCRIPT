89c6fe59d64d2481ae9ef4757f91a8bfee258a04
Amar <113778863+Amar-Hiremath@users.noreply.github.com>
2022-09-21 11:22:38 +0530

    Create Extract amount from string.js

---
+++ Extract amount from string.js
@@ -0,0 +1,15 @@
+// Extract the amount out of this string 
+
+let strg = "amar hiremath "
+let amount = strg.slice("amar".length)
+console.log(amount)
+
+// OUTPUT = hiremath
+
+// Another way to do it 
+
+let strg = "amar hiremath "
+let amount = strg.slice(5)
+console.log(amount)
+
+// OUTOUT = hiremath
