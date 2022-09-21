57e83768836ad57342a7751c08f61557f9befddd
Amar <113778863+Amar-Hiremath@users.noreply.github.com>
2022-09-21 10:24:46 +0530

    Create Demonstrating Upper and Lower Case.js

---
+++ Demonstrating Upper and Lower Case.js
@@ -0,0 +1,15 @@
+// Explore UpperCase 
+
+const statement = "hey it's amar , let's catch up tomorrow"
+let statement2 = statement.toUpperCase();
+console.log(statement2)
+
+//OUTPUT = HEY IT'S AMAR , LET'S CATCH UP TOMORROW
+
+// Explore LowerCase
+
+const statement = "Hey it's Amar , Let's Catch Up Tomorrow"
+let statement2 = statement.toLowerCase();
+console.log(statement2)
+
+//OUTPUT = hey it's amar , let's catch up tomorrow
