b4e50b27069acb8f207efb9a4ad929a4cf2c5ec3
Amar <113778863+Amar-Hiremath@users.noreply.github.com>
2022-09-23 22:54:30 +0530

    Create Create arary of square of given numbs.js

---
+++ Create arary of square of given numbs.js
@@ -0,0 +1,8 @@
+// Create an array of square of a given number 
+
+let ary = [1,2,20,30,5,40,8,90,100]
+let nary = ary.map((x)=>{
+ return (x*x)
+})
+console.log(nary)
+// OUTPUT =  [ 1, 4, 400, 900, 25, 1600, 64, 8100, 10000 ]
