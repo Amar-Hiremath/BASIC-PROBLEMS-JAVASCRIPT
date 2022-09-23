be1394802392f4752402323eb03c3a7542b8146e
Amar <113778863+Amar-Hiremath@users.noreply.github.com>
2022-09-23 07:13:37 +0530

    Create Adding numbers to an array from the user.js

---
+++ Adding numbers to an array from the user.js
@@ -0,0 +1,12 @@
+
+// Create an array of numbers and take input from the user to add numbers to the array 
+
+let ary = [1,2,3,4,5,6]
+let num = prompt ("Enter a number") 
+num = Number.parseInt(num)
+ary.push(num)
+console.log(ary)
+
+/*USER INPUT = 100
+*OUTPUT = [1,2,3,4,5,6,100]
+*/
